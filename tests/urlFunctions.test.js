3fd174e0b188a7124f86e4c9ca781de814cd9324
umahari <43908258+umahari@users.noreply.github.com>
2021-01-08 00:53:11 +0530

    Update urlFunctions.test.js

--- tests/urlFunctions.test.js
+++ tests/urlFunctions.test.js
@@ -1,1 +1,8 @@
-
+//import your function:
+const { testUrl } = require('../../url_functions');
+//describe divide our tests in sections
+describe('url module -> testUrl', () => {
+test('passing nothing to function testUrl- EXPECT ERROR MESSAGE',                      async () => {
+await expect(testUrl(null)).rejects.toThrow('Fetch failed');
+});
+}
